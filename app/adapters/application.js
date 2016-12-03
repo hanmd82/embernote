1a84b539a242c8baa60a2674ab840e2f3dcc7a47
Mingding <hanmd82@gmail.com>
2016-12-03 22:35:03 +0800

    feat: Adapting to non-conventional API with session objects

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api'
+  namespace: 'api/alt',
+  headers: function() {
+    if (this.get('session.user')) {
+      return { 'username' : this.get('session.user').get('name') };
+    }
+  }.property('session.user')
 });
