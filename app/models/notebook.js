bc0afb55e6a35f7493576b3f24acd308c19e6a86
Mingding <hanmd82@gmail.com>
2016-12-03 19:08:38 +0800

    chore: Fix typo in model relations

--- app/models/notebook.js
+++ app/models/notebook.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   user: DS.belongsTo('user'),
-  notes: DS.hasMany('notes')
+  notes: DS.hasMany('note')
 });
